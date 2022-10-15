cb77bf000a825872b28487934a7266eba2f3e258
Aran Cox <aran-cox@uiowa.edu>
2022-10-15 10:22:15 -0500

    rely on package for dependencies and creation of directories

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,28 +4,4 @@ class rdbduprunner::install
 )
 {
   ensure_packages($rdbduprunner::packages)
-  file { $rdbduprunner::config_dir:
-    ensure => directory,
-    owner  => $rdbduprunner::owner,
-    group  => $rdbduprunner::group,
-    mode   => '0775',
-  }
-  file { $rdbduprunner::executable:
-    owner  => 'root',
-    group  => 0,
-    mode   => '0555',
-    source => $rdbduprunner::executable_source,
-  }
-  file { "${rdbduprunner::config_dir}/conf.d":
-    ensure => directory,
-    owner  => $rdbduprunner::owner,
-    group  => $rdbduprunner::group,
-    mode   => '0755',
-  }
-  file { ['/var/run/rdbduprunner','/var/log/rdbduprunner']:
-    ensure => directory,
-    owner  => $rdbduprunner::owner,
-    group  => $rdbduprunner::group,
-    mode   => '0770',
-  }
 }
