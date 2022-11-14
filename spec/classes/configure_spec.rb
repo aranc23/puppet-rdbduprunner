4e17f97a1739565294daab6916db4dad408b7539
Aran Cox <aran-cox@uiowa.edu>
2022-11-14 13:51:03 -0600

    remove comment junk

--- spec/classes/configure_spec.rb
+++ spec/classes/configure_spec.rb
@@ -16,26 +16,7 @@ describe 'rdbduprunner::configure' do
       it { is_expected.to contain_file('/etc/rdbduprunner/rdb-excludes').with('ensure' => 'directory', 'owner' => 'root', 'group' => 0, 'mode' => '0775', 'purge' => false) } 
       it { is_expected.to have_rbdduprunner__backupset_resource_count(0) }
       it { is_expected.to have_rbdduprunner__backupdestination_resource_count(0) }
-      
-    end
-    let(:pre_condition) { 'class { rdbduprunner: owner => "bob", group => "jim" }' }
 
+    end
   end
-  # context 'with class params' do
-  #   let(:pre_condition) { "class { rdbduprunner:
-  #     'backupsets' => { 'test' => { 'paths' => [ '/usr' ], 'prerun' => '/bin/true' } },
-  #       'owner' => 'bob', 'group' => 'jim'
-  #         }"}       
-    
-  #   it { should compile }
-  #   it { is_expected.to contain_concat('/etc/rdbduprunner.rc').with('owner' => 'bob', 'group' => 'jim', 'mode' => '0440' ) }
-  #   it { is_expected.to contain_file('/etc/rdbduprunner/excludes').with('ensure' => 'directory', 'owner' => 'bob', 'group' => 'jim', 'mode' => '0775', 'purge' => false) } 
-  #   it { is_expected.to contain_file('/etc/rdbduprunner/rdb-excludes').with('ensure' => 'directory', 'owner' => 'bob', 'group' => 'jim', 'mode' => '0775', 'purge' => false) }
-  #   it { is_expected.not_to contain_rdbduprunner__backupset('test').with(
-  #                             'config_file' => '/etc/rdbduprunner.rc',
-  #                             'concat' => true,
-  #                             'paths' => ['/usr'],
-  #                             'prerun' => '/bin/true',
-  #                           ) }
-  # end
 end
