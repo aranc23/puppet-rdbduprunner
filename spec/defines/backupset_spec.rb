89b8d657fee3436d9135e90352508ef793387cac
Aran Cox <aran-cox@uiowa.edu>
2022-11-14 21:45:32 -0600

    update backupset spec for modernization

--- spec/defines/backupset_spec.rb
+++ spec/defines/backupset_spec.rb
@@ -15,76 +15,109 @@ describe 'rdbduprunner::backupset' do
       it { is_expected.to compile }
 
       it { is_expected.to contain_file("/etc/rdbduprunner/conf.d/backupset-namevar.conf")
-                            .with( 'ensure'  => 'present',
+                            .with('ensure' => 'absent') }
+      it { is_expected.to contain_file("/etc/rdbduprunner/conf.d/backupset-namevar.yaml")
+                            .with(
                                    'owner'   => 'root',
                                    'group'   => 0,
                                    'mode'    => '0440',
-                                   'content' => "<BackupSet namevar>\n</BackupSet>\n",
+                                   'content' => "--- {}\n",
                                  )
       }    
-    end
-    context "concat" do
-      let(:params) do
-        { 'concat' => true }
-      end
-      it { is_expected.to contain_concat__fragment('backupset|namevar|/etc/rdbduprunner/conf.d/backupset-namevar.conf')
-                            .with( 'target' => '/etc/rdbduprunner/conf.d/backupset-namevar.conf',
-                                   'order' => '30-namevar',
-                                   'content' => "<BackupSet namevar>\n</BackupSet>\n",
-                                 )
-      }    
+      it { is_expected.not_to contain_concat__fragment('backupset|namevar|/etc/rdbduprunner/conf.d/backupset-namevar.yaml') }
     end
     context "all params" do
       let(:params) do
         {
-          'config_file' => "/etc/rdbduprunner/conf.d/backupset-not-namevar.conf",
-          'host' => 'example.com',
-          'paths' => ['/usr','/var/'],
-          'skips' => ['not','this'],
-          'skipres' => ['1','2'],
-          'excludes' => ['3','4',],
+          'config_file' => "/etc/rdbduprunner/conf.d/backupset-not-namevar.yaml",
           'allowfs' => ['ext','ext2'],
           'backupdestination' => 'toaster',
-          'inventory' => true,
-          'inplace' => false,
           'checksum' => true,
-          'wholefile' => false,
-          'stats' => true,
-          'prerun' => 'r',
-          'postrun' => 'o',
-          'rtag' => 'tickle',
           'disabled' => true,
+          'duplicitybinary' => '/usr/local/bin/duplicity',
+          'exclude' => '1',
+          'excludes' => ['3','4',],
+          'host' => 'example.com',
+          'inplace' => false,
+          'inventory' => true,
+          'maxage' => '4d',
           'maxinc' => 4,
+          'path' => ['/var/'],
+          'paths' => ['/usr'],
+          'postrun' => 'o',
+          'prerun' => 'r',
+          'rdiffbackupbinary' => '/usr/local/bin/rdiff-backup',
+          'rsyncbinary' => '/local/rsync',
+          'skip' => ['a'],
+          'skips' => ['not','this'],
+          'skipfstype' => 'zfs',
+          'skipre' => ['4','3'],
+          'skipres' => ['1','2'],
+          'sshcompress' => true,
+          'stats' => false,
+          'rtag' => 'tickle',
+          'trickle' => 5,
+          'tricklebinary' => '/bin/trickle',
+          'useagent' => true,
+          'verbosity' => 5,
+          'volsize' => 1000,
+          'wholefile' => false,
+          'zfsbinary' => '/usr/sbin/zfs',
         }
       end
       it { is_expected.to compile }
-
       it { is_expected.to contain_file("/etc/rdbduprunner/conf.d/backupset-not-namevar.conf")
-                            .with( 'ensure'  => 'present',
-                                   'owner'   => 'root',
-                                   'group'   => 0,
-                                   'mode'    => '0440',
-                                   'content' => "<BackupSet namevar>
-  BackupDestination toaster
-  Disabled true
-  PreRun r
-  PostRun o
-  Host example.com
-  Inventory true
-  Tag tickle
-  Inplace false
-  Path /usr
-  Path /var/
-  Exclude 3
-  Exclude 4
-  SkipRE 1
-  SkipRE 2
-  Skip not
-  Skip this
-  AllowFS ext
-  AllowFS ext2
-  MaxInc 4
-</BackupSet>
+                            .with('ensure' => 'absent') }
+
+      it { is_expected.to contain_file("/etc/rdbduprunner/conf.d/backupset-not-namevar.yaml")
+                            .with(
+                              'owner'   => 'root',
+                              'group'   => 0,
+                              'mode'    => '0440',
+                              'content' => "---
+allowfs:
+- ext
+- ext2
+backupdestination: toaster
+checksum: true
+disabled: true
+duplicitybinary: \"/usr/local/bin/duplicity\"
+exclude:
+- '1'
+- '3'
+- '4'
+host: example.com
+inplace: false
+inventory: true
+maxage: 4d
+maxinc: 4
+path:
+- \"/usr\"
+- \"/var/\"
+postrun: o
+prerun: r
+rdiffbackupbinary: \"/usr/local/bin/rdiff-backup\"
+rsyncbinary: \"/local/rsync\"
+skip:
+- a
+- not
+- this
+skipfstype: zfs
+skipre:
+- '1'
+- '2'
+- '3'
+- '4'
+sshcompress: true
+stats: false
+tag: tickle
+trickle: 5
+tricklebinary: \"/bin/trickle\"
+useagent: true
+verbosity: 5
+volsize: 1000
+wholefile: false
+zfsbinary: \"/usr/sbin/zfs\"
 ")
       }    
       
